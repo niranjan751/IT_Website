9b305d791c9359b9395160a3ee95350efe742949
niranjan751 <niranjanspeed5@gmail.com>
2026-06-27 09:47:55 +0530

    recovered

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './Home/home'
-import Enterprise from './Components/Enterprise/EnterPrise'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Enterprise />
+    <Home />
   </StrictMode>,
 )
